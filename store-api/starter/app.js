e15acbf440723eff379b71428c6c8ff2b2dbc1b7
king610160 <king610160@gmail.com>
2023-12-08 13:52:41 +0800

    feat: set mongoDB, make two controllers, and related routes.

--- store-api/starter/app.js
+++ store-api/starter/app.js
@@ -1,1 +1,41 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async errors
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+const router = require('./routes/products')
+
+// middleware, if need to change respond to json, need to add .json()
+app.use(express.json())
+
+// router
+app.get('/', (req,res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use('/api/v1/products', router)
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+
+const start = async () => {
+    try {
+        //connect DB
+        await connectDB(process.env.MONGODB_URI)
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
