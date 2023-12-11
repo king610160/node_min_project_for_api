b831d3cf56be2eeddb14cf484f919db713cfd3a7
king610160 <king610160@gmail.com>
2023-12-11 13:51:33 +0800

    feat: modified error-handle to certain error to correspond to correct status code.

---
+++ JWT-Basics/starter/errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./badrequest-error')
+const UnAuthticatedError = require('./unauthticated-error')
+
+module.exports = {
+    CustomAPIError,
+    BadRequestError,
+    UnAuthticatedError
+}
