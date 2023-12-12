8b69d83b19c5bc9acc4ede17decf50a850c9339c
king610160 <king610160@gmail.com>
2023-12-12 18:16:34 +0800

    feat: finish all jobs route.

--- jobs-api/starter/controllers/jobs.js
+++ jobs-api/starter/controllers/jobs.js
@@ -1,21 +1,57 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError, NoPermissionError } = require('../errors')
+
 const getAllJobs = async (req, res) => {
-    res.send(`Get all jobs`)
+    const jobs = await Job
+        .find({ createdBy: req.user.userId })
+        .sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
-    res.send(`Get job`)
+    const id = req.params.id
+    const job = await Job.findById(id)
+    if (!job) throw new NotFoundError(`There is no job with ${id}`)
+
+    const userId = req.user.userId
+
+    if (userId !== String(job.createdBy)) throw new NoPermissionError('You have no permission to enter the page.')
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
-    res.send(`Create jobs`)
+    req.body.createdBy = req.user.userId
+    const result = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json(result)
 }
 
 const updateJob = async (req, res) => {
-    res.send(`Update job`)
+    const { company, position } = req.body
+    if (!company || !position) throw new BadRequestError('Need to enter with company and postion.')
+
+    const _id = req.params.id
+    const userId = req.user.userId
+
+    const job = await Job.findOneAndUpdate( 
+        { _id, createdBy: userId }, 
+        req.body, 
+        {new: true, runValidators: true}
+    )
+    if (!job) throw new NotFoundError(`There is no job with ${_id}`)
+    
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJobs = async (req, res) => {
-    res.send(`Delete jobs`)
+    const _id = req.params.id
+    const userId = req.user.userId
+
+    const job = await Job.findOneAndRemove( 
+        { _id, createdBy: userId }
+    )
+    if (!job) throw new NotFoundError(`There is no job with ${_id}`)
+    res.status(StatusCodes.OK).json({ deleteJob: job })
 }
 module.exports = {
     getAllJobs, getJob, createJob, updateJob, deleteJobs
--- jobs-api/starter/errors/index.js
+++ jobs-api/starter/errors/index.js
@@ -2,10 +2,12 @@ const CustomAPIError = require('./custom-api')
 const UnauthenticatedError = require('./unauthenticated')
 const NotFoundError = require('./not-found')
 const BadRequestError = require('./bad-request')
+const NoPermissionError = require('./no-permission')
 
 module.exports = {
   CustomAPIError,
   UnauthenticatedError,
   NotFoundError,
   BadRequestError,
+  NoPermissionError
 }
