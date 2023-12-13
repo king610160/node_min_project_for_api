1c31e0c1706edf77c9e3e7ac5809d8a63209a33d
king610160 <king610160@gmail.com>
2023-12-13 14:40:51 +0800

    feat: add swagger.yaml for api document.

--- jobs-api/starter/app.js
+++ jobs-api/starter/app.js
@@ -3,6 +3,17 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// use api document
+const YAML = require('yamljs')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDoc = YAML.load('./swagger.yaml')
+
+// extra security
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
 // connectDB
 const connectDB = require('./db/connect')
 
@@ -17,16 +28,32 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter)
 app.use(express.json());
-// extra packages
-
-// routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', auth, jobsRouter)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+  res.send('<h1>Jobs API</h1><a href="/api-doc">Document</a>')
+})
+
+// use api document at here
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerDoc) )
+
+// routes
+app.use('/api/v1/auth', limiter, authRouter)
+app.use('/api/v1/jobs', auth, limiter, jobsRouter)
+
+
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
